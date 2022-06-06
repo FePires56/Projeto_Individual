54c49979bcb05f33ba177cf5e8d81d509c46d03b
FePires56 <felipe.pires@sptech.school>
2022-06-06 15:47:04 -0300

    Atualizações no site

--- src/database/script-tabelas.sql
+++ src/database/script-tabelas.sql
@@ -1,60 +1,90 @@
--- Arquivo de apoio, caso você queira criar tabelas como as aqui criadas para a API funcionar.
--- Você precisa executar os comandos no banco de dados para criar as tabelas,
--- ter este arquivo aqui não significa que a tabela em seu BD estará como abaixo!
+create database PlayCloud;
+use PlayCloud;
 
-/* para workbench - local - desenvolvimento */
-CREATE DATABASE acquatec;
 
-USE acquatec;
+create table votacao (
+idVotacao int PRIMARY KEY AUTO_INCREMENT,
+nomeCategoria varchar(45)
+)AUTO_INCREMENT = 100;
 
-CREATE TABLE usuario (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	nome VARCHAR(50),
-	email VARCHAR(50),
-	senha VARCHAR(50)
-);
-
-CREATE TABLE aviso (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	titulo VARCHAR(100),
-    descricao VARCHAR(150),
-	fk_usuario INT,
-	FOREIGN KEY (fk_usuario) REFERENCES usuario(id)
-); 
-
-CREATE TABLE medida (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	temperatura DOUBLE,
-	umidade DOUBLE,
-	momento DATETIME,
-	fk_aquario INT
+create table Usuario (
+idUsuario int primary key auto_increment,
+nome varchar (45),
+email varchar (45),
+senha varchar (45),
+fkConvidado int,
+constraint Usuario_fkConvidado foreign key (fkConvidado) references Usuario (idUsuario),
+fkVotacao int,
+constraint Usuario_fkVotacao foreign key (fkVotacao) references votacao (idVotacao),
+acessoJogos boolean default 0
 );
 
 
-
-
-/* para sql server - remoto - produção */
-
-CREATE TABLE usuario (
-	id INT PRIMARY KEY IDENTITY(1,1),
-	nome VARCHAR(50),
-	email VARCHAR(50),
-	senha VARCHAR(50),
-);
-
-CREATE TABLE aviso (
-	id INT PRIMARY KEY IDENTITY(1,1),
-	titulo VARCHAR(100),
-    descricao VARCHAR(150),
-	fk_usuario INT FOREIGN KEY REFERENCES usuario(id)
-); 
-
-CREATE TABLE medida (
-	id INT PRIMARY KEY IDENTITY(1,1),
-	temperatura DECIMAL,
-	umidade DECIMAL,
-	momento DATETIME,
-	fk_aquario INT
+create table mensalidade (
+fkUsuario int,
+idMensalidade int,
+primary key (fkUsuario, idMensalidade),
+dataMensalidade date,
+dataVencimento date,
+valor decimal (5,2)
 );
 
 
+DESC Usuario;
+
+insert into votacao(nomeCategoria) value 
+('Ação e aventura'),
+('Ação'),
+('Terror'),
+('Estratégia'),
+('Esportes'),
+('Diversão'),
+('FPS'),
+('RPG');
+
+INSERT INTO Usuario VALUES
+
+(null, 'fefe', 'fe@g.com', '123', null, 102, null),
+(null, 'user1', '1@g.com', '123', null, 100, null),
+(null, 'user2', '2@g.com', '123', null, 100, null),
+(null, 'user3', '3@g.com', '123', null, 100, null),
+(null, 'user4', '4@g.com', '123', null, 101, null),
+(null, 'user5', '5@g.com', '123', null, 101, null),
+(null, 'user6', '6@g.com', '123', null, 102, null),
+(null, 'user7', '7@g.com', '123', null, 102, null),
+(null, 'user8', '8@g.com', '123', null, 102, null),
+(null, 'user9', '9@g.com', '123', null, 103, null),
+(null, 'user10', '10@g.com', '123', null, 104, null),
+(null, 'user11', '11@g.com', '123', null, 104, null),
+(null, 'user11', '12@g.com', '123', null, 105, null),
+(null, 'user11', '13@g.com', '123', null, 106, null),
+(null, 'user11', '14@g.com', '123', null, 106, null),
+(null, 'user11', '15@g.com', '123', null, 106, null),
+(null, 'user11', '16@g.com', '123', null, 106, null),
+(null, 'user11', '17@g.com', '123', null, 107, null),
+(null, 'user12', '18@g.com', '123', null, 107, null);
+
+
+
+
+
+
+select * from Usuario;
+
+select count(FkVotacao) from Usuario where fkVotacao = 100;
+
+select count(FkVotacao) from Usuario where fkVotacao = 101;
+
+select count(FkVotacao) from Usuario where fkVotacao = 102;
+
+select count(FkVotacao) from Usuario where fkVotacao = 103;
+
+select count(FkVotacao) from Usuario where fkVotacao = 104;
+
+select count(FkVotacao) from Usuario where fkVotacao = 105;
+
+select count(FkVotacao) from Usuario where fkVotacao = 106;
+
+select count(FkVotacao) from Usuario where fkVotacao = 107;                                                               
+
+select nomeCategoria, count(idUsuario) as QtdVotos from Usuario, votacao where FkVotacao = idvotacao GROUP BY fkVotacao ORDER BY fkVotacao;
--- src/models/medidaModel.js
+++ src/models/medidaModel.js
@@ -1,32 +1,47 @@
 var database = require("../database/config");
 
-function buscarUltimasMedidas (limite_linhas) {
-    
-    instrucaoSql = ''
-    
+function buscarUltimasMedidas(limite_linhas) {
 
-if (process.env.AMBIENTE_PROCESSO == "desenvolvimento") {
-        instrucaoSql = `select count(FkVotacao) as QtdRPG from Usuario where fkVotacao = 107;`;
-    } else {
-        console.log("\nO AMBIENTE (produção OU desenvolvimento) NÃO FOI DEFINIDO EM app.js\n");
-        return
-    }
-    
-    console.log("Executando a instrução SQL: \n" + instrucaoSql);
-    return database.executar(instrucaoSql);
+    instrucaoSql = ''
+
+
+    if (process.env.AMBIENTE_PROCESSO == "desenvolvimento") {
+        instrucaoSql = `select count(idUsuario) as QtdVotos from Usuario GROUP BY fkVotacao ORDER BY fkVotacao;`;
+        console.log("Executando a instrução SQL: \n" + instrucaoSql);
+         
+ }
+        else {
+            console.log("\nO AMBIENTE (produção OU desenvolvimento) NÃO FOI DEFINIDO EM app.js\n");
+            return
+        }
+        console.log("Executando a instrução SQL: \n" + instrucaoSql);
+        return database.executar(instrucaoSql);
 }
 
 function buscarMedidasEmTempoReal() {
-    
+
     instrucaoSql = ''
-    
- if (process.env.AMBIENTE_PROCESSO == "desenvolvimento") {
-        instrucaoSql = `select 
-                        temperatura, 
-                        umidade, DATE_FORMAT(momento,'%H:%i:%s') as momento_grafico, 
-                        fk_aquario 
-                        from medida where fk_aquario = 
-                    order by id desc limit 1`;
+
+    if (process.env.AMBIENTE_PROCESSO == "desenvolvimento") {
+        instrucaoSql = `select count(FkVotacao) as QtdAcaoAvent from Usuario where fkVotacao = 100;`;
+        console.log("Executando a instrução SQL: \n" + instrucaoSql);
+         database.executar(instrucaoSql);
+         instrucaoSql = `select count(FkVotacao) as QtdAcao from Usuario where fkVotacao = 101;`;
+
+         instrucaoSql = `select count(FkVotacao) as QtdTerror from Usuario where fkVotacao = 102;`;
+        console.log("Executando a instrução SQL: \n" + instrucaoSql);
+         database.executar(instrucaoSql);
+         instrucaoSql = `select count(FkVotacao) as QtdEstrategia from Usuario where fkVotacao = 103;`;
+
+         instrucaoSql = `select count(FkVotacao) as QtdEsporte from Usuario where fkVotacao = 104;`;
+        console.log("Executando a instrução SQL: \n" + instrucaoSql);
+         database.executar(instrucaoSql);
+         instrucaoSql = `select count(FkVotacao) as QtdDiversao from Usuario where fkVotacao = 105;`;
+
+         instrucaoSql = `select count(FkVotacao) as QtdFPS from Usuario where fkVotacao = 106;`;
+        console.log("Executando a instrução SQL: \n" + instrucaoSql);
+         database.executar(instrucaoSql);
+         instrucaoSql = `select count(FkVotacao) as QtdRPG from Usuario where fkVotacao = 107;`;
     } else {
         console.log("\nO AMBIENTE (produção OU desenvolvimento) NÃO FOI DEFINIDO EM app.js\n");
         return
